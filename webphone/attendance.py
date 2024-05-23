80e39ec658c8e86f479a027cc11252c20034bb8f
xitongsys <xitongsys@gmail.com>
2024-05-23 08:03:31 +0800

    ..

--- webphone/attendance.py
+++ webphone/attendance.py
@@ -1,10 +1,15 @@
 #%%
 
 import time
+from datetime import datetime
 
 from adb import Adb
 
 if __name__ == '__main__':
+    now = datetime.now()
+    if now.hour not in [7,8,18,19]:
+        exit(0)
+
     phone = Adb(r'C:\Users\zxt\AppData\Local\Android\Sdk\platform-tools\adb.exe')
 
     for i in range(4):
